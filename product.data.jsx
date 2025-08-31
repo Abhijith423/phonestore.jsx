3058b8e080aae9ef51c6782d36cf7668bd435afd
Abhijith <abhijithemes@gmail.com>
2025-08-31 12:54:39 +0530

    product.data.jsx

---
+++ product.data.jsx
@@ -0,0 +1,39 @@
+export const phonesData = [
+  {
+    id: 1,
+    model: "Apple I Phone",
+    desc: "Apple iPhone 15 Pro 256GB",
+    price: 999,
+    image: "apple"
+  },
+  {
+    id: 2,
+    model: "Nothing Phone",
+    desc: "nothing 3a 5G 256GB",
+    price: 799,
+    image: "nothing"
+  },
+  {
+    id: 3,
+    model: "pixel ",
+    desc: "pixel 9 pro 256GB",
+    price: 800,
+    image: "pixel"
+  },
+    {
+    id: 4,
+    model: "redmi ",
+    desc: "redmi note 12 pro 256GB",
+    price: 699,
+    image: "redmi"
+  },
+    {
+    id: 5,
+    model: "samsung",
+    desc: "samsung s25 pro 256GB",
+    price: 799,
+    image: "samsung"
+  }
+];
+export default phonesData;
+
